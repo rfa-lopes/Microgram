1288a8e8846d28837bd7abe8db6f54d1cc583a5e
AJ <al.jesus@campus.fct.unl.pt>
2019-05-27 18:10:59 +0100

    Update já passa 2.a)

--- SD2019-Microgram-TP2-Code/create-truststore-mediaclient.sh
+++
@@ -1,3 +0,0 @@
-cp base-truststore.ks client-truststore.ks
-echo "Use password: changeit"
-keytool -importcert -file mediaserver.cert -alias mediastore -keystore client-truststore.ks
--- SD2019-Microgram-TP2-Code/create-truststore.sh
+++ SD2019-Microgram-TP2-Code/create-truststore.sh
@@ -1,3 +1,4 @@
 cp base-truststore.ks client-truststore.ks
 echo "Use password: changeit"
 keytool -importcert -file mediaserver.cert -alias mediastore -keystore client-truststore.ks
+keytool -importcert -file microgramserver.cert -alias microgram -keystore client-truststore.ks
